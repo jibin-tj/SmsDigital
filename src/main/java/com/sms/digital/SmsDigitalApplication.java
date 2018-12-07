cc56558e1e1ab9a6b680de9861d7a9f285f0477a
Jibin <jibin.therethjacob@metronom-external.com>
2018-12-07 20:52:42 +0100

    first commit

---
+++ src/main/java/com/sms/digital/SmsDigitalApplication.java
@@ -0,0 +1,12 @@
+package com.sms.digital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsDigitalApplication.class, args);
+	}
+}
